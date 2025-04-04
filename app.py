8bbabcb93f378fabf8b35c110e280da446044d37
Quinlin Gregg <44556872+TheQantas@users.noreply.github.com>
2025-04-04 17:03:23 -0600

    Misc fixes

--- app.py
+++ app.py
@@ -125,10 +125,9 @@ def reports():
     for category in categories:
         category["total_inventory"] = category_inventory[category["id"]]
 
-    flag = True
     value = request.args.get('value')
     return render_template("reports_index.html", product_list=products, user=current_user,
-                           categories=categories, quant=[c["total_inventory"] for c in categories], flag=flag, value=value)
+                           categories=categories, quant=[c["total_inventory"] for c in categories], value=value)
 
 
 @app.get("/<int:product_id>")
